b41379d29447fc3dcbad7694dc954bcedb268900
정순인 <tnsdls1966@naver.com>
2020-05-10 23:33:46 +0900

    cc

---
+++ card.py
@@ -0,0 +1,101 @@
+suits = 'CDHS'
+ranks = '23456789TJQKA'
+
+from abc import ABCMeta, abstractmethod
+
+class Card(metaclass=ABCMeta):
+    """Abstact class for playing cards
+    """
+    def __init__(self, rank_suit):
+        if rank_suit[0] not in ranks or rank_suit[1] not in suits:
+            raise ValueError(f'{rank_suit}: illegal card')
+        self.card = rank_suit
+
+    def __repr__(self):
+        return self.card
+    
+    @abstractmethod
+    def value(self):
+        """Subclasses should implement this method
+        """
+        raise NotImplementedError("value method not implemented")
+    @property
+    def rank(self) :
+        return self.card[0]
+
+    @property
+    def suit(self) :
+        return self.card[1]
+    # card comparison operators
+    def __gt__(self, other): return self.value() > other.value()
+    def __ge__(self, other): return self.value() >= other.value()
+    def __lt__(self, other): return self.value() < other.value()
+    def __le__(self, other): return self.value() <= other.value()
+    def __eq__(self, other): return self.value() == other.value()
+    def __ne__(self, other): return self.value() != other.value()
+
+
+
+class PKCard(Card):
+    """Card for Poker game
+    """
+    VALUES = dict(zip(ranks, range(2, 2+len(ranks))))
+
+    def value(self) :
+        return PKCard.VALUES[self.card[0]]
+
+if __name__ == '__main__':
+    c1 = PKCard('QC')
+    c2 = PKCard('9D')
+    c3 = PKCard('9C')
+    print(f'{c1} {c2} {c3}')
+    # comparison
+    print(c1 > c2 == c3)
+
+    # sorting
+    cards = [c1, c2, c3, PKCard('AS'), PKCard('2D')]
+    sorted_cards = sorted(cards)
+    print(sorted_cards)
+    cards.sort()
+    print(cards)
+
+
+import random
+class Deck():
+    def __init__(self, cls):
+        self.cards = [cls(r+s) for r in ranks for s in suits]
+
+    def __str__(self) :
+        return str(self.cards)
+    
+    def __len__(self) :
+        return len(self.cards)
+    
+    def __getitem__(self, index) :
+        return self.cards[index]
+    
+    def shuffle(self) :
+        random.shuffle(self.cards)
+    
+    def pop(self) :
+        return self.cards.pop()
+
+if __name__ == '__main__':
+    deck = Deck(PKCard)  # deck of poker cards
+    deck.shuffle()
+    c = deck[0]
+    print('A deck of', c.__class__.__name__)
+    print(deck)
+    # testing __getitem__ method
+    print(deck[-5:])
+
+    while len(deck) >= 10:
+        my_hand = []
+        your_hand = []
+        for i in range(5):
+            for hand in (my_hand, your_hand):
+                card = deck.pop()
+                hand.append(card)
+        my_hand.sort(reverse=True)
+        your_hand.sort(reverse=True)
+        print(my_hand, '>', your_hand, '?', my_hand > your_hand)
